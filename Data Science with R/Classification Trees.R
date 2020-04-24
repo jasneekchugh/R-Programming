827458d49284d15cfeee9798102fdc8edaa96b60
jasneekchugh <chughjasneek@gmail.com>
2020-04-24 01:36:22 -0400

    Classification Trees

---
+++ Data Science with R/Classification Trees.R
@@ -0,0 +1,57 @@
+                            #Classification Trees
+
+
+# Load the rpart package
+library(rpart)
+
+head(loans)
+str(loans)
+# Build a lending model predicting loan outcome versus loan amount and credit score
+loan_model <- rpart(outcome~loan_amount+ credit_score, data = loans, method = "class", control = rpart.control(cp = 0))
+
+# Make a prediction for someone with good credit
+predict(loan_model, good_credit, type = "class")
+
+
+# Examine the loan_model object
+loan_model
+
+# Load the rpart.plot package
+library(rpart.plot)
+
+# Plot the loan_model with default settings
+rpart.plot(loan_model)
+
+# Plot the loan_model with customized settings
+rpart.plot(loan_model, type = 3, box.palette = c("red", "green"), fallen.leaves = TRUE)
+
+# Determine the number of rows for training
+nrow(loans) * 0.75
+
+# Create a random sample of row IDs
+sample_rows <- sample(nrow(loans), nrow(loans) * 0.75)
+
+# Create the training dataset
+loans_train <- loans[sample_rows, ]
+
+# Create the test dataset
+loans_test <- loans[-sample_rows, ]
+
+
+# Grow a tree using all of the available applicant data
+loan_model <- rpart(outcome ~ ., data = loans_train, method = "class", control = rpart.control(cp = 0))
+
+# Make predictions on the test dataset
+loans_test$pred <- predict(loan_model, loans_test, type = "class")
+
+# Examine the confusion matrix
+table(loans_test$pred, loans_test$outcome)
+
+# Compute the accuracy on the test dataset
+mean(loans_test$pred == loans_test$outcome)
+
+
+
+
+
+
