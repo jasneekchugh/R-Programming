13a5175fa28182a1e0dcee612ebb433c6ee2109c
jasneekchugh <chughjasneek@gmail.com>
2020-04-24 01:46:28 -0400

    Classification Trees

--- Data Science with R/Classification Trees.R
+++ Data Science with R/Classification Trees.R
@@ -51,7 +51,14 @@ table(loans_test$pred, loans_test$outcome)
 mean(loans_test$pred == loans_test$outcome)
 
 
+#preventing overgrown trees
+# Grow a tree with maxdepth of 6
+loan_model <- rpart(outcome ~ ., data = loans_train, method = "class", control = rpart.control(cp = 0, maxdepth = 6))
 
+# Make a class prediction on the test set
+loans_test$pred <- predict(loan_model, loans_test, type = "class")
 
+# Compute the accuracy of the simpler tree
+mean(loans_test$pred == loans_test$outcome)
 
 
